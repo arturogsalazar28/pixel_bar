5439bc8ac1a63e1a4696064cbd8045f39cfdd444
arturos-gxg <arturos@genebygene.com>
2018-12-07 14:06:55 -0600

    Adding button and switching through colors

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import board
 import neopixel
 import time
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
-pixels = neopixel.NeoPixel(board.D18, 30, brightness=1)
+colorButton = 10
+
+colorCounter = 0
 
 colorList = [
     (255,108,17),
@@ -26,7 +29,34 @@ colorList = [
     (84,19,136)
 ]
 
-for color in colorList:
-    pixels.fill(color) 
+def button_callback(channel):
+    global colorCounter
+
+    # Fill the pixels with the color in the list
+    pixels.fill(colorList[colorCounter]) 
     pixels.show()
-    time.sleep(2)
+    
+    # Once we reach the last item in the list start back at the first
+    # if not go to the next
+    if colorCounter == (len(colorList)-1):
+        colorCounter = 0
+    else:
+        colorCounter = colorCounter + 1
+
+ # Use physical pin numbering for button
+GPIO.setmode(GPIO.BOARD)
+
+# Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
+
+# Setup pixel strip using neopixel library
+pixels = neopixel.NeoPixel(board.D18, 30, brightness=1) 
+
+# Setup event on pin 10 rising edge
+GPIO.add_event_detect(colorButton,GPIO.RISING,callback=button_callback) 
+
+# Run until someone presses enter
+message = input("Press enter to quit...\n\n") 
+
+# Clean up
+GPIO.cleanup()
