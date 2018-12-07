6ecc85ad36367c5b51ce6af2bc3875285a17cbe6
arturos-gxg <arturos@genebygene.com>
2018-12-07 09:22:02 -0600

    Adding a color list and making it switch between all the colors

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 import board
 import neopixel
+import time
 
 pixels = neopixel.NeoPixel(board.D18, 30, brightness=1)
 
-pixels.fill((255,0,0)) 
+colorList = [
+    (255,108,17),
+    (255,56,100),
+    (45,226,230),
+    (38,20,71),
+    (2,55,136),
+    (101,13,137),
+    (146,0,117),
+    (246,1,157),
+    (212,0,120),
+    (36,23,52),
+    (46,33,87),
+    (253,55,119),
+    (247,6,207),
+    (253,29,83),
+    (249,100,14),
+    (255,67,101),
+    (84,13,110),
+    (121,30,148),
+    (84,19,136)
+]
 
+for color in colorList:
+    pixels.fill(color) 
+    pixels.show()
+    time.sleep(2)
